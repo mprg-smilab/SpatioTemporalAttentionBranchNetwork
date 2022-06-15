23d41ee3de11f85123de9f4da3c6e4953cf4e8fd
thirakawa <hirakawa@mprg.cs.chubu.ac.jp>
2022-06-15 18:04:56 +0900

    [update] update Docker

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,8 @@ chmod a+x entrypoint.sh
 
 
 # build
-docker build --tag=cumprg/stabn:1.10.0 --force-rm=true --file=./Dockerfile .
+docker build --tag=cumprg/stabn:1.10.0 --force-rm=true --file=./Dockerfile_1_10_0 .
+docker build --tag=cumprg/stabn:1.11.0 --force-rm=true --file=./Dockerfile_1_11_0 .
 
 
 echo "Build docker; done."
